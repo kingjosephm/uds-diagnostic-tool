781933cdcc22816fdada9dd035017773b2f042bf
Joseph King <jking@luxoft.com>
2025-01-08 12:50:00 +0100

    updated engineer_data.py script with better understanding of structure of reply and request codes. Re-output xlsx files

--- preprocessing/engineer_data.py
+++ preprocessing/engineer_data.py
@@ -6,8 +6,6 @@ import pandas as pd
 
 if __name__ == '__main__':
     
-    restrict_uds_only = False
-    
     dir_contents = [i for i in os.listdir("./data") if ".pcap" in i]
     
     for file in dir_contents:
@@ -21,29 +19,26 @@ if __name__ == '__main__':
             
             if packet.highest_layer.split('_')[0] in ['UDS']:
                 
-                
                 packet_info = {
                     'number': packet.number,  # Packet number
                     'timestamp': packet.sniff_time.strftime("%Y-%m-%d %H:%M:%S"),  # Time of packet capture
                     'protocol': packet.highest_layer.split('_')[0],  # overall protocol
-                    'length': packet.length,
                     'source': packet.doip.source_address if hasattr(packet.doip, 'source_address') else 'N/A',
                     'target': packet.doip.target_address if hasattr(packet.doip, 'target_address') else 'N/A',
-                    'doip': packet.doip_raw.value,
-                    'sid': packet.uds.sid if hasattr(packet.uds, 'sid') else 'N/A',
-                    'reply': packet.uds.reply if hasattr(packet.uds, 'reply') else 'N/A',
+                    'request': True if packet.uds.reply == '0x00' else False,  # misnomer, this is request code if '0x00'. Reply codes are '0x01'
+                    'sid': 'N/A',  # Service ID, fill in below
+                    'error': 'N/A',  # alter below, if present
                 }
                 
-                # Separate the DOIP and UDS data into bytes and capitalize the letters
-                for key in ['doip']:
-                    if packet_info[key] != 'N/A':
-                        packet_info[key] = ':'.join([
-                            "0x" + ''.join([char.upper() if char.isalpha() else char for char in packet_info[key][i:i+2]])
-                            for i in range(0, len(packet_info[key]), 2)
-                        ])
+                if packet_info['request']:
+                    packet_info['sid'] = packet.uds.sid
+                else:  # reply code
+                    packet_info['sid'] = hex(int(packet.uds.sid, 16) + 0x40)  # must add '0x40' to get correct reply code for both positive and negative replies
+                    if hasattr(packet.uds, 'err'):
+                        packet_info['error'] = packet.uds.err.code  # negative reply error code. This is second byte of the negative reply shown in CloudShark. Ignore first byte of negative reply code for now since this not standardized
                 
                 # Capitalize the letters in the SID and Reply fields
-                for key in ['sid', 'reply']:  # assumes only one byte
+                for key in ['sid', 'error']:  # assumes only one byte
                     if packet_info[key] != 'N/A':
                         packet_info[key] = ''.join([char.upper() if char.isalpha() and char != 'x' else char for char in packet_info[key]])                
                     
